23b463d911ee1edcce1c6e0ba9f1836037df5f8b
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2022-04-04 09:32:16 -0700

    Added descriptive text and signup sheet

--- pick-up-yo-shit-sd/src/App.js
+++ pick-up-yo-shit-sd/src/App.js
@@ -1,15 +1,23 @@
 import fingerPoint from ".//fingerPoint.jpg";
+import litterSd from ".//litterSd.jpg";
 import "./App.css";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={fingerPoint} className="App-logo" alt="logo" />
+        <background
+          img
+          src={litterSd}
+          className="App-background"
+          alt="litterpic"
+        />
         <p>
           <>Pick up yo SHIT SD!</>
         </p>
-        <h1 strong> Hi, We're P . U . Y . S San Diego</h1>
+        <h1 strong> Hi, We're P . U . Yo . S . San Diego</h1>
         <h2>
           My wife and neice LOVE to go hiking. We go almost every weekend and
           couldnt help but notice all the f#$^@#*ng trash lying around! WTF
@@ -17,7 +25,7 @@ function App() {
         </h2>
         <h3>
           With 'do not litter signs' and trash cans aplenty, We still see tons
-          of trash on every outing. Thats why we started Pick up yo Shit SD!
+          of trash on every outing!!! Thats why we started Pick up yo Shit SD!
         </h3>
         <h4>
           We're tired of seeing the trash everywhere! So we're gonna do
@@ -31,6 +39,7 @@ function App() {
           Interested in joining? Sign up below! Click the link for events near
           you! Welcome to Pick Up Yo SHIT SD!
         </h6>
+        <Signup />
       </header>
     </div>
   );
