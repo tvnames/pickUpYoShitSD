03f8206bd7538dcad3ac94648b935a5c4ef47d16
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2022-04-16 16:38:00 -0700

    Added calander for scheduling pickups

---
+++ pick-up-yo-shit-sd/src/components/Events.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Calendar from "react-calendar";
+//import "react-calendar/dist/Calendar.css";
+import "..Css/Calendar.css";
+
+class CalendarComponent extends Component {
+  setClass = (date) => {
+    const dateobj =
+      this.props.eventsList &&
+      this.props.eventsList.find((x) => {
+        return (
+          date.getDay() === new Date(x.start).getDay() &&
+          date.getMonth() === new Date(x.start).getMonth() &&
+          date.getDate() === new Date(x.start).getDate()
+        );
+      });
+    return dateobj ? dateobj.colorName : "";
+  };
+  render() {
+    return (
+      <Calendar
+        tileClassName={({ activeStartDate, date, view }) => this.setClass(date)}
+      />
+    );
+  }
+}
+
+export default CalendarComponent;
