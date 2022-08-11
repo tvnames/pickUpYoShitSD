6216c7ca2909f43852c9f151cba649ab3d5f9b3b
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2022-08-10 18:22:06 -0700

    edited wording and structure of page

--- pick-up-yo-shit-sd/src/App.js
+++ pick-up-yo-shit-sd/src/App.js
@@ -15,31 +15,33 @@ function App() {
           <>Pick up yo SHIT SD!</>
         </p>
         <Nav />
-        <h1 strong> Hi, We're Pick Up Yo Sh$* San Diego</h1>
+        <h1> Hi, We're Pick Up Yo Sh!% San Diego</h1>
       </header>
-
       <h1>
-        My wife and niece LOVE to go hiking. We go almost every weekend and
-        couldn't help but notice all the f#$^@#*ng trash lying around! WTF SD?!?
+        My wife, niece, and LOVE to go hiking. We go almost every weekend and
+        couldn't help but notice all the F#$^@#*NG trash lying around!
       </h1>
+      <h1> WTF SD?!?</h1>
       <h2>
-        With 'do not litter signs' and trash cans aplenty, We still see tons of
-        trash on every outing!!! Thats why we started Pick up yo Shit SD!
+        With 'do not litter signs' and trash cans EVERYWHERE, Please pick up
+        your trash is not getting the desired result.
       </h2>
+      <h2>Thats why we started Pick up yo Shit SD!</h2>
       <h3>
-        We're tired of seeing the trash everywhere! So we're gonna do something
+        We're tired of seeing the trash everywhere and we're gonna do something
         about it!
       </h3>
       <h4>
         Every 2nd and 4th weekend of each month we will pick one local San Diego
-        outdoor recreational area to clean the F*$@ UP!
+        outdoor recreational area to clean it the F*$@ UP!
       </h4>
       <h5>
         Interested in joining? Sign up below! Click the link for events near
         you! Welcome to Pick Up Yo SHIT SD!
       </h5>
-
-      <Signup />
+      <h6>
+        <Signup />
+      </h6>
     </div>
   );
 }
